6863e4d254cb8be7d66555d85019d14819e688a0
SUDHIR KUMAR <157155704+sudhirkumar12345@users.noreply.github.com>
2026-02-03 23:42:04 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,14 @@
+// write a program to print area of circle
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radis ");
+        int radius;
+        radius=sc.nextInt();
+        double pi = 3.14;
+        double area = pi*radius*radius;
+        System.out.println("the area of circle is :" +area);
+
+    }
+}
