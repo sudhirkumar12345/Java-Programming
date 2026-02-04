20dd7570935b7b73efbe7fa507f5444712a68a32
SUDHIR KUMAR <157155704+sudhirkumar12345@users.noreply.github.com>
2026-02-05 00:58:30 +0530

    Switch Case Statement
    The switch statement in Java is a multi-way decision statement that executes code based on the value of an expression. It is a cleaner alternative to an if-else-if ladder and supports types like byte, short, char, int, long, enum, String, and wrapper classes.

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number 1 to 7 : ");
+        int n = sc.nextInt();   // input taken
+
+        switch (n) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid input! Enter 1 to 7 only.");
+        }
+
+        sc.close();
+    }
+}
